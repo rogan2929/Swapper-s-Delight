315fce65c70a1d4a3c721828d4bc9fc3de2dbae0
rogan2929 <richard.scales@live.com>
2014-01-27 11:57:31 -0800

    *

--- php/new-posts.php
+++ php/new-posts.php
@@ -26,8 +26,6 @@ $queries = array(
 	'imageQuery' => 'SELECT object_id,images FROM photo WHERE object_id IN (SELECT attachment FROM #streamQuery)'
 );
 
-echo json_encode($queries);
-
 $response = $fbSession->api(array(
 	'method' => 'fql.multiquery',
 	'queries' => $queries
