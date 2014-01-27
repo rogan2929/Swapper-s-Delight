a26b10c9c520f9aa5adc78597c13692d2888c5b8
rogan2929 <richard.scales@live.com>
2014-01-27 14:39:29 -0800

    0

--- php/post-details.php
+++ php/post-details.php
@@ -8,7 +8,7 @@ if ($postId == null) {
 	$postId = '120696471425768_256930841135663';
 }
 
-$query = 'SELECT post_id,message,actor_id,permalink,like_info,share_info,comment_info,tagged_ids FROM stream WHERE post_id=' . $postId;
+$query = 'SELECT post_id,message,actor_id,permalink,like_info,share_info,comment_info,tagged_ids FROM stream WHERE post_id="' . $postId . '"';
 
 $response = $fbSession->api(array(
 	'method' => 'fql.query',
