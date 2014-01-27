c65b064c9e59938e8762f02e13d484ad60986bc2
rogan2929 <richard.scales@live.com>
2014-01-27 13:05:58 -0800

    0

--- php/new-posts.php
+++ php/new-posts.php
@@ -47,7 +47,7 @@ for ($i = 0; $i < count($stream); $i++) {
 			for ($j = 0; $j < count($images); $j++) {
 				if ($post['attachment']['media'][0]['photo']['fbid'] == $images[$j]['object_id']) {
 					//$post['image_url'] = $images[$j][4]['source'];
-					echo json_encode($images[$j]);
+					echo json_encode($images[$j]['images'][4]['source']);
 					//echo json_encode($post);
 					break;
 				}
