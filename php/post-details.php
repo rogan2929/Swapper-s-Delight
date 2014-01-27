bd1155e48a011bf0bd436a230f33f2da36392bbe
rogan2929 <richard.scales@live.com>
2014-01-27 15:40:30 -0800

    0

--- php/post-details.php
+++ php/post-details.php
@@ -7,7 +7,7 @@ $postId = $_GET['postId'];
 $queries = array(
 	'detailsQuery' => 'SELECT post_id,message,actor_id,permalink,like_info,share_info,comment_info,tagged_ids FROM stream WHERE post_id="' . $postId . '"',
 	'userQuery' => 'SELECT last_name,first_name,pic_square,profile_url FROM user WHERE uid IN (SELECT actor_id FROM #detailsQuery)',
-	'commentsQuery' => 'SELECT fromid,text,text_tags,attachment FROM comment WHERE post_id="' . $postId . '"'
+	'commentsQuery' => 'SELECT fromid,text,text_tags,attachment FROM comment WHERE post_id IN (SELECT post_id FROM #detailsQuery)'
 );
 
 // Run the query.
