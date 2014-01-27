dd7ae081e5375bb113d3d7255f4eb880ce608750
rogan2929 <richard.scales@live.com>
2014-01-27 11:20:06 -0800

    *

--- php/group-info.php
+++ php/group-info.php
@@ -25,7 +25,8 @@ $response = $fbSession->api(array(
 $groups = array();
 
 for ($i = 0; $i < count($response); $i++) {
-	$groups[$i] = $response[$i]->fql_result_set[0];
+	//$groups[$i] = $response[$i]->fql_result_set[0];
+	echo $response[$i]->fql_result_set[0];
 }
 
 // Pass the data on to the client.
