a449163a5f1aa8849f2e597ce6ba8b18c0677c36
rogan2929 <richard.scales@live.com>
2014-01-28 00:10:44 -0800

    0

--- php/post-details.php
+++ php/post-details.php
@@ -7,7 +7,8 @@ $postId = $_GET['postId'];
 $queries = array(
 	'detailsQuery' => 'SELECT post_id,message,actor_id,permalink,like_info,share_info,comment_info,tagged_ids FROM stream WHERE post_id="' . $postId . '"',
 	'userQuery' => 'SELECT last_name,first_name,pic_square,profile_url FROM user WHERE uid IN (SELECT actor_id FROM #detailsQuery)',
-	'commentsQuery' => 'SELECT fromid,text,text_tags,attachment FROM comment WHERE post_id IN (SELECT post_id FROM #detailsQuery)'
+	'commentsQuery' => 'SELECT fromid,text,text_tags,attachment FROM comment WHERE post_id IN (SELECT post_id FROM #detailsQuery)',
+	'commentUserQuery' => 'SELECT last_name,first_name,pic_square,profile_url FROM user WHERE uid IN (SELECT fromid in #commentsQuery)'
 );
 
 // Run the query.
@@ -23,5 +24,6 @@ $postDetails['user'] = $response[2]['fql_result_set'][0];
 
 // Return the result.
 //echo json_encode($postDetails);
-echo json_encode($postDetails);
+//echo json_encode($postDetails);
+echo json_encode($response);
 ?>
