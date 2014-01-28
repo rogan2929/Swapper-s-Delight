95664c7ec3ba3766482426830dbba4d9b5cc8e00
rogan2929 <richard.scales@live.com>
2014-01-28 09:45:38 -0800

    0

--- php/post-details.php
+++ php/post-details.php
@@ -25,5 +25,5 @@ $postDetails['user'] = $response[2]['fql_result_set'][0];
 // Return the result.
 //echo json_encode($postDetails);
 //echo json_encode($postDetails);
-echo json_encode($response);
+echo json_encode($postDetails);
 ?>
