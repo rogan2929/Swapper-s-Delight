506c719c5e25ca412f902f7fe5c7bf9c5df42b18
rogan2929 <richard.scales@live.com>
2014-01-28 14:06:59 -0800

    0

--- js/main.js
+++ js/main.js
@@ -633,7 +633,7 @@ var SwdView = {
 					userImage = '';
 				}
 
-				comment = $('<div class="post-comment ui-corner-all ui-widget ui-widget-content"><div class="ui-state-default"><div class="post-comment-user-image"></div><a href="' + post.comments[i].user.profile_url + '" target="_blank">' + post.comments[i].user.first_name + ' ' + post.comments[i].user.last_name + '</a></div>' + post.comments[i].text + '</div>');
+				comment = $('<div class="post-comment ui-corner-all ui-widget ui-widget-content"><div class="ui-state-default"><div class="post-comment-user-image"></div><a class="post-comment-user-name" href="' + post.comments[i].user.profile_url + '" target="_blank">' + post.comments[i].user.first_name + ' ' + post.comments[i].user.last_name + '</a></div>' + post.comments[i].text + '</div>');
 
 				$(comment).find('.post-comment-user-image').css('background-image', userImage);
 
