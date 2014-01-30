ee1a97cf2c9ef8b0bb555feb40ca61c764b7a8e4
rogan2929 <richard.scales@live.com>
2014-01-29 20:30:56 -0800

    .git

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@
 
 // PostType Enum
 var PostType = {
-	group: 0,
-	myposts: 1,
-	pinned: 2,
-	search: 3
+	group : 0,
+	myposts : 1,
+	pinned : 2,
+	search : 3
 };
 
 // Prod AppId
@@ -27,21 +27,21 @@ var SwdModel = {
 	 * @param {type} gid
 	 * @param {type} callback
 	 */
-	getLikedPosts: function(gid, callback) {
+	getLikedPosts : function(gid, callback) {
 		// TODO: liked-posts.php
 	},
 	/***
 	 * Query database for groups that the user has marked as 'BST' (Buy, Sell, Trade)
 	 * @param {type} callback
 	 */
-	getGroupInfo: function(callback) {
+	getGroupInfo : function(callback) {
 		$.ajax({
-			type: 'GET',
-			url: '/php/group-info.php',
-			success: function(response) {
+			type : 'GET',
+			url : '/php/group-info.php',
+			success : function(response) {
 				callback.call(SwdModel, JSON.parse(response));
 			},
-			fail: function(response) {
+			fail : function(response) {
 
 			}
 		});
@@ -51,14 +51,14 @@ var SwdModel = {
 	 * @param {type} gid
 	 * @param {type} callback
 	 */
-	getMyPosts: function(gid, callback) {
+	getMyPosts : function(gid, callback) {
 		$.ajax({
-			type: 'GET',
-			url: '/php/my-posts.php?gid=' + gid,
-			success: function(response) {
+			type : 'GET',
+			url : '/php/my-posts.php?gid=' + gid,
+			success : function(response) {
 				callback.call(SwdModel, JSON.parse(response.responseText));
 			},
-			fail: function(response) {
+			fail : function(response) {
 
 			}
 		});
@@ -69,7 +69,7 @@ var SwdModel = {
 	 * @param {type} createdTime
 	 * @param {type} callback Completed callback function.
 	 */
-	getNewestPosts: function(gid, createdTime, callback) {
+	getNewestPosts : function(gid, createdTime, callback) {
 		var url = '/php/new-posts.php?gid=' + gid;
 
 		if (createdTime) {
@@ -77,12 +77,12 @@ var SwdModel = {
 		}
 
 		$.ajax({
-			type: 'GET',
-			url: url,
-			success: function(response) {
+			type : 'GET',
+			url : url,
+			success : function(response) {
 				callback.call(SwdModel, JSON.parse(response));
 			},
-			fail: function(response) {
+			fail : function(response) {
 
 			}
 		});
@@ -92,26 +92,26 @@ var SwdModel = {
 	 * @param {type} postId
 	 * @param {type} callback
 	 */
-	getPostDetails: function(postId, callback) {
+	getPostDetails : function(postId, callback) {
 		$.ajax({
-			type: 'GET',
-			url: '/php/post-details.php?postId=' + postId,
-			success: function(response) {
+			type : 'GET',
+			url : '/php/post-details.php?postId=' + postId,
+			success : function(response) {
 				callback.call(SwdModel, JSON.parse(response));
 			},
-			fail: function(response) {
+			fail : function(response) {
 
 			}
 		});
 	},
-	startSession: function(callback) {
+	startSession : function(callback) {
 		$.ajax({
-			type: 'GET',
-			url: '/php/session.php',
-			success: function(response) {
+			type : 'GET',
+			url : '/php/session.php',
+			success : function(response) {
 				callback.call(SwdModel, response);
 			},
-			fail: function(response) {
+			fail : function(response) {
 
 			}
 		});
@@ -122,26 +122,26 @@ var SwdModel = {
  * Presenter for the Swapper's Delight program.
  */
 var SwdPresenter = {
-	oldestPost: null,
-	postType: PostType.group,
-	selectedGroup: null,
-	groups: null,
+	oldestPost : null,
+	postType : PostType.group,
+	selectedGroup : null,
+	groups : null,
 	/**
 	 * Entry point of program.
 	 */
-	init: function() {
+	init : function() {
 		SwdView.initView();
 
 		$.ajaxSetup({
-			cache: true
+			cache : true
 		});
 
 		// Fetch the FB JS API
 		$.getScript('//connect.facebook.net/en_US/all.js', function() {
 			FB.init({
-				appId: AppId,
-				cookie: true,
-				status: true
+				appId : AppId,
+				cookie : true,
+				status : true
 			});
 
 			$('#loginbutton,#feedbutton').removeAttr('disabled');
@@ -152,8 +152,7 @@ var SwdPresenter = {
 					SwdModel.startSession(function() {
 						SwdPresenter.startApp();
 					});
-				}
-				else {
+				} else {
 					FB.login(function(response) {
 						if (response.status === 'connected') {
 							SwdModel.startSession(function() {
@@ -161,7 +160,7 @@ var SwdPresenter = {
 							});
 						}
 					}, {
-						scope: 'user_groups,user_likes'
+						scope : 'user_groups,user_likes'
 					});
 				}
 			});
@@ -170,10 +169,8 @@ var SwdPresenter = {
 	/***
 	 * Starts the application after init has finished.
 	 */
-	startApp: function() {
+	startApp : function() {
 		FB.Canvas.setAutoGrow();
-
-		SwdPresenter.facebookPageInfoPoll();
 
 		// Retrieve group info for logged in user.
 		SwdModel.getGroupInfo(function(response) {
@@ -184,9 +181,9 @@ var SwdPresenter = {
 				SwdView.addGroupsToMenu(SwdPresenter.groups);
 
 				$('#popup-menu-groups').menu().position({
-					of: $('#button-menu-groups'),
-					my: 'left top',
-					at: 'left bottom'
+					of : $('#button-menu-groups'),
+					my : 'left top',
+					at : 'left bottom'
 				});
 
 				// Install Event Handlers
@@ -208,8 +205,7 @@ var SwdPresenter = {
 				});
 
 				SwdView.positionMenus();
-			}
-			else {
+			} else {
 				// Have the view prompt the user to edit BST groups.
 			}
 		});
@@ -218,7 +214,7 @@ var SwdPresenter = {
 	 * Periodically call FB.Canvas.getPageInfo in order to dynmically update the UI within the canvas
 	 * iframe.
 	 */
-	facebookPageInfoPoll: function() {
+	facebookPageInfoPoll : function() {
 		FB.Canvas.getPageInfo(function(pageInfo) {
 			var scrollTop;
 			var offsetTop;
@@ -238,24 +234,25 @@ var SwdPresenter = {
 
 			// TODO: Update right-panel height
 
-			// TODO: Detect scroll at bottom
+			// Detect scroll at bottom
 			if (scrollTop >= $('#app-content').height() - clientHeight) {
-				alert('bottom reached.');
+				SwdPresenter.loadNewestPosts(true);
+			} else {
+				// Only do another call if we are not loading more posts.
+				setTimeout(SwdPresenter.facebookPageInfoPoll, 50);
 			}
-
-			setTimeout(SwdPresenter.facebookPageInfoPoll, 50);
 		});
 	},
 	/***
 	 * Load posts liked by user.
 	 */
-	loadLikedPosts: function() {
+	loadLikedPosts : function() {
 
 	},
 	/***
 	 * Load posts owned by user.
 	 */
-	loadMyPosts: function() {
+	loadMyPosts : function() {
 		SwdModel.getMyPosts(SwdPresenter.currentUid, SwdPresenter.selectedGroup.gid, SwdPresenter.accessToken, function(response) {
 			alert(response.responseText);
 		});
@@ -264,18 +261,12 @@ var SwdPresenter = {
 	 * Load feed for the current group.
 	 * @param {type} loadNextPage
 	 */
-	loadNewestPosts: function(loadNextPage) {
+	loadNewestPosts : function(loadNextPage) {
 		var createdTime;
-
-		// TODO: configure options based on what tab the user is on.
-		if (SwdPresenter.postType === PostType.search) {
-
-		}
 
 		if (loadNextPage) {
 			createdTime = SwdPresenter.oldestPost.created_time;
-		}
-		else {
+		} else {
 			createdTime = null;
 		}
 
@@ -290,11 +281,12 @@ var SwdPresenter = {
 			if (response) {
 				SwdPresenter.oldestPost = response[response.length - 1];
 				SwdView.populatePosts(response);
-			}
-			else
-			if (!loadNextPage) {
+			} else if (!loadNextPage) {
 				SwdPresenter.oldestPost = null;
 			}
+			
+			// Start polling page info again.
+			SwdPresenter.facebookPageInfoPoll();
 		});
 	},
 	/***
@@ -302,48 +294,47 @@ var SwdPresenter = {
 	 * @param {type} id
 	 * @param {type} link
 	 */
-	sendFacebookMessage: function(id, link) {
+	sendFacebookMessage : function(id, link) {
 		FB.ui({
-			to: id,
-			method: 'send',
-			link: link
+			to : id,
+			method : 'send',
+			link : link
 		});
 	},
 	/***
 	 * Set currently selected group.
 	 * @param {type} group
 	 */
-	setSelectedGroup: function(group) {
+	setSelectedGroup : function(group) {
 		SwdPresenter.selectedGroup = group;
 		SwdPresenter.loadNewestPosts(false);
 		SwdView.setGroupButtonText(group.name);
 	},
 	// Event Handlers (onX(e, args))
-	onClickButtonNew: function(e, args) {
+	onClickButtonNew : function(e, args) {
 		SwdView.showNewPostDialog();
 	},
-	onClickHtml: function(e, args) {
+	onClickHtml : function(e, args) {
 		SwdView.closeAllUiMenus();
 		SwdView.hideRightPanel();
 	},
-	onClickMenuButton: function(e, args) {
+	onClickMenuButton : function(e, args) {
 		SwdView.showUiMenu(e);
 	},
-	onClickMenuItemGroup: function(e, args) {
+	onClickMenuItemGroup : function(e, args) {
 		var id = $(e.currentTarget).attr('id');
 
 		if (id === 'menu-item-choose-groups') {
 			// TODO: Display group chooser dialog.
-		}
-		else {
+		} else {
 			// Set selected group and load its feed.
 			SwdPresenter.setSelectedGroup(id);
 		}
 	},
-	onClickMenuItemMain: function(e, args) {
+	onClickMenuItemMain : function(e, args) {
 		var id = $(e.currentTarget).attr('id');
 	},
-	onClickNavButton: function(e, args) {
+	onClickNavButton : function(e, args) {
 		var id = $(e.currentTarget).attr('id');
 
 		switch (id) {
@@ -366,14 +357,14 @@ var SwdPresenter = {
 
 		SwdView.setSelectedPostType(id);
 	},
-	onClickPanelMessageUser: function(e, args) {
+	onClickPanelMessageUser : function(e, args) {
 		var profileUrl = $(e.currentTarget).data('src');
 		window.open(profileUrl);
 	},
-	onClickPostButton: function(e, args) {
+	onClickPostButton : function(e, args) {
 		SwdPresenter.sendFacebookMessage('', 'http://www.foxnews.com');
 	},
-	onClickPostTile: function(e, args) {
+	onClickPostTile : function(e, args) {
 		var id;
 		var post;
 
@@ -394,14 +385,13 @@ var SwdPresenter = {
 
 			if (post) {
 				SwdView.showPostDetails(post);
-			}
-			else {
+			} else {
 				// TODO: Do a real error message.
 				alert('Unable to display post. It was most likely deleted.');
 			}
 		});
 	},
-	onScrollGroupFeed: function(e, args) {
+	onScrollGroupFeed : function(e, args) {
 		// Check to see if the user has scrolled all the way to the bottom.
 		if (($(e.currentTarget).scrollTop() + $(e.currentTarget).innerHeight()) >= (e.currentTarget.scrollHeight)) {
 			SwdPresenter.loadNewestPosts(true);
@@ -409,7 +399,7 @@ var SwdPresenter = {
 
 		// TODO: Scroll up to refresh.
 	},
-	onWindowResize: function(e, args) {
+	onWindowResize : function(e, args) {
 		SwdView.positionMenus();
 	},
 };
@@ -418,12 +408,12 @@ var SwdPresenter = {
  * View for the Swapper's Delight program.
  */
 var SwdView = {
-	handlers: {},
+	handlers : {},
 	/***
 	 * Add group to Group Select Menu.
 	 * @param {type} groups
 	 */
-	addGroupsToMenu: function(groups) {
+	addGroupsToMenu : function(groups) {
 		var i = 0;
 
 		for ( i = 0; i < groups.length; i++) {
@@ -433,49 +423,49 @@ var SwdView = {
 	/**
 	 * Init function for SwdView.
 	 */
-	initView: function() {
+	initView : function() {
 		// Set up buttons
 		$('.button-nav').button();
 
 		$('#button-menu-main').button({
-			icons: {
-				primary: 'ui-icon-gear'
+			icons : {
+				primary : 'ui-icon-gear'
 			}
 		});
 
 		$('#button-menu-groups').button({
-			icons: {
-				primary: 'ui-icon-contact'
+			icons : {
+				primary : 'ui-icon-contact'
 			}
 		});
 
 		$('#button-new').button({
-			icons: {
-				primary: 'ui-icon-comment'
+			icons : {
+				primary : 'ui-icon-comment'
 			}
 		});
 
 		$('#button-menu-daysback').button({
-			icons: {
-				primary: 'ui-icon-calendar'
+			icons : {
+				primary : 'ui-icon-calendar'
 			}
 		});
 
 		$('#post-button-comment').button({
-			icons: {
-				primary: 'ui-icon-comment'
+			icons : {
+				primary : 'ui-icon-comment'
 			}
 		});
 
 		$('#post-button-pm').button({
-			icons: {
-				primary: 'ui-icon-mail-closed'
+			icons : {
+				primary : 'ui-icon-mail-closed'
 			}
 		});
 
 		$('#post-button-like').button({
-			icons: {
-				primary: 'ui-icon-pin-s'
+			icons : {
+				primary : 'ui-icon-pin-s'
 			}
 		});
 
@@ -504,36 +494,36 @@ var SwdView = {
 	 * @param {type} selector
 	 * @param {type} event
 	 */
-	installHandler: function(name, handler, selector, event) {
+	installHandler : function(name, handler, selector, event) {
 		this.handlers[name] = handler;
 
 		$(selector).bind(event, function(e, args) {
 			SwdView.handlers[name].call(SwdPresenter, e, args);
 		});
 	},
-	clearPosts: function() {
+	clearPosts : function() {
 		$('#group-feed').empty();
 	},
 	/***
 	 * Closes all Jquery UI menus.
 	 */
-	closeAllUiMenus: function() {
+	closeAllUiMenus : function() {
 		$('.ui-menu').hide();
 	},
 	/***
 	 * Removes the right panel from view.
 	 */
-	hideRightPanel: function() {
+	hideRightPanel : function() {
 		$('#right-panel').hide('slide', {
-			easing: 'easeInOutQuint',
-			direction: 'right'
+			easing : 'easeInOutQuint',
+			direction : 'right'
 		}, 300);
 	},
 	/***
 	 * Write posts to the page.
 	 * @param {type} posts
 	 */
-	populatePosts: function(posts) {
+	populatePosts : function(posts) {
 		var i;
 		var isEmpty;
 		var url;
@@ -553,15 +543,13 @@ var SwdView = {
 				if (post.message) {
 					message = post.message;
 					//message = post.created_time;
-				}
-				else {
+				} else {
 					message = null;
 				}
 
 				if (post.image_url) {
 					url = post.image_url;
-				}
-				else {
+				} else {
 					url = null;
 				}
 
@@ -573,19 +561,14 @@ var SwdView = {
 					$(postTile).addClass('post-tile-multi');
 					$(primaryContent).css('background-image', 'url("' + url + '")');
 					$(secondaryContent).text(message);
-				}
-				else
-				if (message && !url) {
+				} else if (message && !url) {
 					$(postTile).addClass('post-tile-multi');
 					$(primaryContent).css('background-image', 'url("/img/no-image.jpg")');
 					$(secondaryContent).text(message);
-				}
-				else
-				if (!message && url) {
+				} else if (!message && url) {
 					$(postTile).addClass('post-tile-image');
 					$(primaryContent).css('background-image', 'url("' + url + '")');
-				}
-				else {
+				} else {
 					isEmpty = true;
 				}
 
@@ -620,19 +603,19 @@ var SwdView = {
 	 * Sets menu positions.
 	 * @param {type} offset
 	 */
-	positionMenus: function(offset) {
+	positionMenus : function(offset) {
 		$('.menu-button').each(function() {
 			var menu = $(this).find('a').attr('href');
 
 			$(menu).css({
-				top: 0 + offset,
-				left: 0
+				top : 0 + offset,
+				left : 0
 			});
 
 			$(menu).position({
-				of: $(this),
-				my: 'left top',
-				at: 'left bottom'
+				of : $(this),
+				my : 'left top',
+				at : 'left bottom'
 			});
 		});
 	},
@@ -640,7 +623,7 @@ var SwdView = {
 	 * Simulate the placing of fixed divs within the FB app canvas.
 	 * @param {type} offset
 	 */
-	setFixedDivs: function(offset) {
+	setFixedDivs : function(offset) {
 		$('#left-rail, #right-panel').css('top', Math.max(offset + 60, 0));
 		$('#main-toolbar').css('top', Math.max(offset, 0));
 	},
@@ -648,32 +631,32 @@ var SwdView = {
 	 * Dynamically calculate the height of #right-panel, based on how large the FB canvas is.
 	 * @param {type} height
 	 */
-	setRightPanelHeight: function(height) {
+	setRightPanelHeight : function(height) {
 	},
 	/***
 	 * Changes the text shown in the "Select a Group" button.
 	 * @param {type} text Text to display inside the button.
 	 */
-	setGroupButtonText: function(text) {
+	setGroupButtonText : function(text) {
 		$('#button-menu-groups span a').text(text);
 	},
-	setSelectedPostType: function(id) {
+	setSelectedPostType : function(id) {
 		$('.button-nav').removeClass('selected-nav');
 		$('#' + id).addClass('selected-nav');
 	},
 	/***
 	 * Displays new post dialog box.
 	 */
-	showNewPostDialog: function() {
+	showNewPostDialog : function() {
 		$('#dialog-new-post').dialog({
-			modal: true
+			modal : true
 		});
 	},
 	/***
 	 * Shows the post details for the selected post.
 	 * @param {type} post Post to load into right column.
 	 */
-	showPostDetails: function(post) {
+	showPostDetails : function(post) {
 		var userImage;
 		var postImage;
 		var i;
@@ -686,8 +669,7 @@ var SwdView = {
 			$('#post-no-image').hide();
 			$('#post-image').show();
 			$('#post-image').css('background-image', postImage);
-		}
-		else {
+		} else {
 			// Show the no-image notification.
 			$('#post-permalink').attr('href', post.permalink).text(post.permalink);
 			$('#post-image').hide();
@@ -696,8 +678,7 @@ var SwdView = {
 
 		if (post.user.pic_square) {
 			userImage = 'url("' + post.user.pic_square + '")';
-		}
-		else {
+		} else {
 			userImage = '';
 		}
 
@@ -716,8 +697,7 @@ var SwdView = {
 				// Set user image
 				if (post.comments[i].user.pic_square) {
 					userImage = 'url("' + post.comments[i].user.pic_square + '")';
-				}
-				else {
+				} else {
 					userImage = '';
 				}
 
@@ -737,17 +717,17 @@ var SwdView = {
 	/***
 	 * Brings the right panel into view.
 	 */
-	showRightPanel: function() {
+	showRightPanel : function() {
 		$('#right-panel').show('slide', {
-			easing: 'easeInOutQuint',
-			direction: 'right'
+			easing : 'easeInOutQuint',
+			direction : 'right'
 		}, 300);
 	},
 	/***
 	 * Shows a Jquery UI menu.
 	 * @param {type} e
 	 */
-	showUiMenu: function(e) {
+	showUiMenu : function(e) {
 		var menu;
 
 		e.stopPropagation();
@@ -757,16 +737,16 @@ var SwdView = {
 
 		// Display the menu.
 		$(menu).show('slide', {
-			direction: 'up',
-			duration: 300,
-			easing: 'easeInOutQuint'
+			direction : 'up',
+			duration : 300,
+			easing : 'easeInOutQuint'
 		});
 	}
 };
 
 $(document).ready(function() {
 	$.ajaxSetup({
-		cache: true
+		cache : true
 	});
 	SwdPresenter.init();
 });
