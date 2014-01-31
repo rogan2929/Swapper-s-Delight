43ea433bbb1448207f229cd31c11a01b2e2a46e4
Richard Scales <richard.scales@live.com>
2014-01-31 07:39:17 -0800

    0

--- php/new-posts.php
+++ php/new-posts.php
@@ -1,57 +1,25 @@
 <?php
 
 require_once 'session.php';
+require_once 'queries.php';
 
 // Retrieve group id that is being queried.
 $gid = $_GET['gid'];
 $updatedTime = $_GET['updatedTime'];
 
-// Base query
-$streamQuery = 'SELECT post_id,updated_time,message,attachment,comment_info FROM stream WHERE source_id=' . $gid;
+$constraints = array();
 
 // For FQL pagination (query by posts with created_time less than created_time of last query's oldest post.)
 if ($updatedTime) {
-	$streamQuery .= ' AND updated_time < ' . $updatedTime;
+	// Add to the constraints array.
+	$constraints[] = array(
+		'field' => 'updated_time',
+		'operator' => '<',
+		'value' => $updatedTime
+	);
 }
 
-// Fetch 30 results, and sorted by creation time.
-$streamQuery .= ' ORDER BY updated_time DESC LIMIT 20';
-
-$queries = array(
-	'streamQuery' => $streamQuery,
-	'imageQuery' => 'SELECT object_id,images FROM photo WHERE object_id IN (SELECT attachment FROM #streamQuery)'
-);
-
-$response = $fbSession->api(array(
-	'method' => 'fql.multiquery',
-	'queries' => $queries
-));
-
-$posts = array();
-
-$stream = $response[0]['fql_result_set'];
-$images = $response[1]['fql_result_set'];
-
-for ($i = 0; $i < count($stream); $i++) {
-	$post = $stream[$i];
-	
-	$post['image_url'] = null;
-	
-	// For posts with an image, look for associate image data.
-	if ($post['attachment'] && $post['attachment']['media'] &&
-		$post['attachment']['media'][0] && $post['attachment']['media'][0]['photo']) {
-			for ($j = 0; $j < count($images); $j++) {
-				if ($post['attachment']['media'][0]['photo']['fbid'] == $images[$j]['object_id']) {
-					$post['image_url'][] = $images[$j]['images'][3]['source'];
-					$post['image_url'][] = $images[$j]['images'][6]['source'];
-					break;
-				}
-			}
-	}
-		
-	// Add to the posts array.
-	$posts[] = $post;
-}
+$posts = streamQuery($gid, 20, $constraints)
 
 // Return the result.
 echo json_encode($posts);
---
+++ php/queries.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once 'session.php';
+
+/***
+ * Reusable funciton that executes an FQL query against the given stream.
+ */
+function streamQuery($sourceId, $limit = 20, $constraints) {
+	$streamQuery = 'SELECT post_id,updated_time,message,attachment,comment_info FROM stream WHERE source_id=' . $sourceId;
+
+	if ($constraints) {
+		//$streamQuery .= $constraints;
+		for ($i = 0; $i < count($constraints); $i++) {
+			$constraint = $constraints[$i];
+			$streamQuery .= ' AND ' . $constraint['field'] . ' ' . $constraint['operator'] . ' ' . $constraint['value'];
+		}
+	}
+
+	// Fetch 20 results, and sorted by creation time.
+	$streamQuery .= ' ORDER BY updated_time DESC LIMIT '. $limit;
+
+	$queries = array(
+		'streamQuery' => $streamQuery,
+		'imageQuery' => 'SELECT object_id,images FROM photo WHERE object_id IN (SELECT attachment FROM #streamQuery)'
+	);
+
+	$response = $fbSession->api(array(
+		'method' => 'fql.multiquery',
+		'queries' => $queries
+	));
+
+	$posts = array();
+
+	$stream = $response[0]['fql_result_set'];
+	$images = $response[1]['fql_result_set'];
+
+	for ($i = 0; $i < count($stream); $i++) {
+		$post = $stream[$i];
+
+		$post['image_url'] = null;
+
+		// For posts with an image, look for associate image data.
+		if ($post['attachment'] && $post['attachment']['media'] && $post['attachment']['media'][0] && $post['attachment']['media'][0]['photo']) {
+			for ($j = 0; $j < count($images); $j++) {
+				if ($post['attachment']['media'][0]['photo']['fbid'] == $images[$j]['object_id']) {
+					$post['image_url'][] = $images[$j]['images'][3]['source'];
+					$post['image_url'][] = $images[$j]['images'][6]['source'];
+					break;
+				}
+			}
+		}
+
+		// Add to the posts array.
+		$posts[] = $post;
+	}
+	return $posts;
+}
+?>
