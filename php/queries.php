264001c72d91e3046f440ebdec2593b612a124ea
Richard Scales <richard.scales@live.com>
2014-01-31 09:04:04 -0800

    0

--- php/queries.php
+++ php/queries.php
@@ -21,6 +21,10 @@ function streamQuery($fbSession, $sourceId, $limit = 20, $constraints) {
 		'streamQuery' => $streamQuery,
 		'imageQuery' => 'SELECT object_id,images FROM photo WHERE object_id IN (SELECT attachment FROM #streamQuery)'
 	);
+	
+	$permissions = $fbSession->api('/me/permissions');
+	
+	echo json_encode($permissions);
 
 	$response = $fbSession->api(array(
 		'method' => 'fql.multiquery',
