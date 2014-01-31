c17205d465a5ad6ba7ab9694b0bb313bb9b94ac1
rogan2929 <richard.scales@live.com>
2014-01-31 00:33:30 -0800

    0

--- php/post-details.php
+++ php/post-details.php
@@ -38,11 +38,9 @@ for ($i = 0; $i < count($postDetails['comments']); $i++) {
 }
 
 // Query action links for the given post. (FQL's action_links column always returns null. Suspect a bug.)
-//$response = $fbSession->api('/"' . $postId . '""?fields=actions');
+$response = $fbSession->api('/"' . $postId . '"?fields=actions');
 
-
-
-//$postDetails['action_links'] = $response['actions'];
+$postDetails['action_links'] = $response['actions'];
 
 // Return the result.
 echo json_encode($postDetails);
