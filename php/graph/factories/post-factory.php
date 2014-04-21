fb0829bc867122922046cdec9fa59a650b4e1aa1
scalesr <scalesr@HpPav.amc.uwmedicine.org>
2014-04-21 10:02:02 -0700

    Merge branch 'dev'
    Conflicts:
    	php/include/dal.php

--- php/graph/factories/post-factory.php
+++ php/graph/factories/post-factory.php
@@ -319,7 +319,8 @@ class PostFactory extends BaseFactory {
             
             $ch = curl_init();
             curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch,CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HEADER, false);
             $this->stream = json_decode(curl_exec($ch));
             curl_close($ch);
 
